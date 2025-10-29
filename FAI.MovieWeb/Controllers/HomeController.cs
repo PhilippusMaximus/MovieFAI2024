52f38c5b24220eaa8a42e971af1d78c2c955336b
PhilippusMaximus <philipp.rheinberger@gmail.com>
2025-10-29 17:32:11 +0100

    UI mit MVC Razor und zweisprachig

---
+++ FAI.MovieWeb/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+using FAI.MovieWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Text;
+
+namespace FAI.MovieWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string HelloWorld(int id)
+        {
+            var sb = new StringBuilder();
+            for(int i = 0; i< id; i++)
+            {
+                sb.AppendLine($"Habe die Ehre {i}\r\n");
+            }
+
+            return sb.ToString();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ FAI.MovieWeb/Program.cs
@@ -0,0 +1,81 @@
+using FAI.Application.Extensions;
+using FAI.MovieWeb.Data;
+using FAI.Persistence.Extensions;
+using FAI.Persistence.Repositories.DBContext;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace FAI.MovieWeb
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            var connectionStringIdentity = builder.Configuration.GetConnectionString("IdentityDbContext") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(connectionStringIdentity));
+            builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+            var connectionStringMovie = builder.Configuration.GetConnectionString("MovieDbContextProduction") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+            builder.Services.AddDbContext<MovieDbContext>(options =>
+                options.UseSqlServer(connectionStringMovie));
+            builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+            builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+            builder.Services.AddControllersWithViews();
+
+            builder.Services.RegisterRepositories();
+            builder.Services.RegisterServices();
+
+            // Definition der Sprachermittlung der Anwendung
+            builder.Services.Configure<RequestLocalizationOptions>(options =>
+            {
+                var supportedCultures = new[] { "en", "de", "fr" };
+
+                options.SetDefaultCulture(supportedCultures[1])
+                       .AddSupportedCultures(supportedCultures)
+                       .AddSupportedUICultures(supportedCultures)
+                       .DefaultRequestCulture = new Microsoft.AspNetCore.Localization.RequestCulture("de");
+            });
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseMigrationsEndPoint();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapStaticAssets();
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Movies}/{action=Index}/{id?}")
+                .WithStaticAssets();
+            app.MapRazorPages()
+               .WithStaticAssets();
+
+            var options = app.Services.GetService<IOptions<RequestLocalizationOptions>>();
+            app.UseRequestLocalization(options.Value);
+
+            app.Run();
+        }
+    }
+}
